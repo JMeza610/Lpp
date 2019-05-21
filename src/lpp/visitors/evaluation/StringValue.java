2995a0a314631633425b9ba271ef8fed39c80c16
IvanSibilla <sibillaivan96@gmail.com>
2019-05-21 11:45:34 +0200

    Completate:
    Classe StringValue

--- src/lpp/visitors/evaluation/StringValue.java
+++ src/lpp/visitors/evaluation/StringValue.java
@@ -6,6 +6,10 @@ public class StringValue extends PrimValue<String> {
         super(value);
     }
 
+    public String asString() {
+        return value;
+    }
+
     @Override
     public final boolean equals(Object obj) {
         if (this == obj)
@@ -13,10 +17,5 @@ public class StringValue extends PrimValue<String> {
         if (!(obj instanceof StringValue))
             return false;
         return value.equals(((StringValue) obj).value);
-    }
-
-    @Override
-    public String asString() {
-        return value;
     }
 }
